5cd5196ae5cefbb6d1e2abcc306a21a856df8872
Pedro-Brito-09 <pe.brito09@gmail.com>
2024-10-17 22:10:59 -0300

    Resolução BubbleSort

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,3 +1,17 @@
 
 def bubble_sort(lista: list) -> None:
-    pass
+    changed = False
+
+    for i in range(len(lista) - 1):
+        current = lista[i]
+        _next = lista[i + 1]
+
+        if current > _next:
+            lista[i] = _next
+            lista[i + 1] = current
+            changed = True
+    
+    if changed:
+        return bubble_sort(lista)
+    
+    return lista
