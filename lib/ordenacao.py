eb57ac0c94395c2ac3e20dc309e4e818e102f5bb
Guilherme P. Santos <Guilhermepiresdossantos70@gmail.com>
2024-10-18 14:43:14 -0300

    retorna None

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,17 +1,21 @@
 
-def bubble_sort(vector: list) -> list:
-    def compare(a, b):
-        if a > b:
-            return b, a 
-        return a, b
+def bubble_sort(lista: list) -> None:
+    def sort(vector: list) -> list:
+        def compare(a: float, b: float):
+            if a > b:
+                return b, a 
+            return a, b
+        
+        for i in range(len(vector)-1):
+            vector[i],vector[i+1] = compare(vector[i],vector[i+1])
+        
+        vector_len = len(vector)
+        
+        if vector_len <= 2:
+            return vector
+        return sort(vector[:vector_len-1])+ (vector[vector_len-1:])
     
-    for i in range(len(vector)-1):
-        vector[i],vector[i+1] = compare(vector[i],vector[i+1])
-    
-    vector_len = len(vector)
-    
-    if vector_len <= 2:
-        return vector
-    return bubble_sort(vector[:vector_len-1])+ (vector[vector_len-1:])
+    lista = sort(lista)
+
 
 #  Guilherme P. Santos
