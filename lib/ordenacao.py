854540bc29e7aaa195e7a767823599705de25002
Guilherme P. Santos <Guilhermepiresdossantos70@gmail.com>
2024-10-18 14:02:39 -0300

    Function bubble_sort() added

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,3 +1,17 @@
 
-def bubble_sort(lista: list) -> None:
-    pass
+def bubble_sort(vector: list) -> list:
+    def compare(a, b):
+        if a > b:
+            return b, a 
+        return a, b
+    
+    for i in range(len(vector)-1):
+        vector[i],vector[i+1] = compare(vector[i],vector[i+1])
+    
+    vector_len = len(vector)
+    
+    if vector_len <= 2:
+        return vector
+    return bubble_sort(vector[:vector_len-1])+ (vector[vector_len-1:])
+
+#  Guilherme P. Santos
