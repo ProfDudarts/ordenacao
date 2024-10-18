e3d657a91e39c92ba2f75bc899e83937f4b57609
EA - Leonardo Rossetti Francatto <leonardo_rossetti@educadventista.org>
2024-10-17 22:51:37 -0300

    Implementação do Bubble Sort

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,3 +1,7 @@
 
 def bubble_sort(lista: list) -> None:
-    pass
+    for i in range(len(lista)):
+        for j in range(len(lista)):
+            if lista[j] > lista[i]:
+                lista[j], lista[i] = lista[i], lista[j]
+    return lista
