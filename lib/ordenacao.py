2bc1289047b6e27c3862993e602e389f26f9e83a
Luix <lugasoal@gmail.com>
2024-10-24 20:57:45 -0300

    add assinatura da função selection sort

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -5,7 +5,10 @@ def bubble_sort(lista):
         for j in range(0, x-i-1):
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                print(lista)
                 sorteado = True
         if not sorteado:
             break
 
+def selection_sort():
+    pass
