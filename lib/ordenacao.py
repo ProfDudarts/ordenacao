6333e68f58877872abb1f5918ec8ed2832ac37c3
Hudson <oficialcontahud@gmail.com>
2024-10-24 22:34:02 -0300

    added sort

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -10,5 +10,24 @@ def bubble_sort(lista):
         if not sorteado:
             break
 
-def selection_sort():
-    pass
+lista = [5,3,4,2,1]
+
+def selection_sort(lista):
+    
+    for i in range(len(lista)):
+
+        min_index = i
+        
+        for j in range(i +1, len(lista)):
+
+            if lista[j] < lista[min_index]:
+
+                min_index = j
+        
+        lista[i], lista[min_index] = lista[min_index], lista[i]
+
+    return lista
+
+
+
+print(selection_sort(lista))
