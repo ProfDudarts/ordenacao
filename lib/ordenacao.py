38a8db1e9bef7e6d0cbb53d47b5d7fc032695e67
Guilherme P. Santos <Guilhermepiresdossantos70@gmail.com>
2024-10-24 22:05:17 -0300

    . . .

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -15,7 +15,5 @@ def selection_sort(vector: list) -> None:
     for i in range(len(vector)): 
         limite += 1
         for j in range(limite, len(vector)):
-            range_ = range(limite, len(vector))
-            menor_j, maior_i = vector[j],vector[i]
             if vector[j] < vector[i]:    
                 vector[j], vector[i] = vector[i], vector[j]
