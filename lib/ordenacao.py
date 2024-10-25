88788d63faff83896e6c93274921833377f9269a
Rafael-lindquist <rafaelblemail@gmail.com>
2024-10-24 22:20:52 -0300

    resolucao de conflito

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,8 +1,22 @@
-def bubble_sort(lista: list) -> None:
-    """Essa função organiza uma lista por Bubble sort"""
+def bubble_sort(lista):
+    x = len(lista)
+    for i in range(x):
+        sorteado = False
+        for j in range(0, x-i-1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                print(lista)
+                sorteado = True
+        if not sorteado:
+            break
+
+def selection_sort(lista: list) -> None:
+    n = len(lista)
+    for i in range(n):
+        min = i
+        for j in range(i+1, n):
+            if lista[j] < lista[min]:
+                min = j
+        lista[i], lista[min] = lista[min], lista[i]
+    print(lista)
     
-    for i in range(len(lista)):
-        for j in range(len(lista)-1):
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
-    return lista
